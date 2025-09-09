443d98e85c54a21bb2465fa63dff5fb5e6a4328b
CS&A-Software <clint.seewald@gmail.com>
2025-09-09 11:26:17 -0600

    Add files via upload
    Auctioneer's Ledger Version 1.0.94.1 - Fix
    
    • Fixed the broken discord link.

--- Constants.lua
+++ Constants.lua
@@ -1,6 +1,6 @@
 -- Auctioneer's Ledger - Constants
 -- This file contains all the static configuration values for the addon.
-AL.VERSION = "1.0.94" 
+AL.VERSION = "1.0.94.1" 
 
 -- Core numerical constants (independent)
 AL.COL_PADDING = 5
